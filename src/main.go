7e27b0716f7d5ab28adfac88be27288051b932fc
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-07-26 17:36:30 +0900

    float64 入れたり。

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/bits"
 	"os"
 	"strconv"
@@ -16,18 +17,66 @@ func nonsenseU64(s0 uint64) uint64 {
 		if i < s0 {
 			s := 1 + nonsenseU64(s0-i)
 			r += m * bits.RotateLeft64(s, int(i))
+			r += r/m + r/(m+1) + r/(m+2)
 		}
 	}
 	return r
-
 }
 
-func test(key uint64) {
+func testOneU64(key uint64) (float64, uint64) {
 	start := time.Now()
 	result := nonsenseU64(key)
 	end := time.Now()
 	ms := float64(end.Sub(start)/time.Nanosecond) * 1e-6
-	fmt.Printf("%d,%f,%d\n", key, ms, result)
+	return ms, result
+}
+
+func testU64(key uint64) {
+	minMs := 1e100
+	result := uint64(0)
+	for i := 0; i < 5; i++ {
+		ms, r := testOneU64(key)
+		result = r
+		minMs = math.Min(minMs, ms)
+	}
+	fmt.Printf("u64,%d,%f,%d\n", key, minMs, result)
+}
+
+func nonsenseF64(s0 float64) float64 {
+	r := 0.0
+	m := 0.0
+	for i := 1.0; i < 64.0; i++ {
+		m += 7
+		if i < s0 {
+			s := nonsenseF64(s0 - i)
+			r += m / (s*s + 1.0)
+		}
+	}
+	return r
+}
+
+func testOneF64(key float64) (float64, float64) {
+	start := time.Now()
+	result := nonsenseF64(key)
+	end := time.Now()
+	ms := float64(end.Sub(start)/time.Nanosecond) * 1e-6
+	return ms, result
+}
+
+func testF64(key float64) {
+	minMs := 1e100
+	result := 0.0
+	for i := 0; i < 5; i++ {
+		ms, r := testOneF64(key)
+		result = r
+		minMs = math.Min(minMs, ms)
+	}
+	fmt.Printf("f64,%.0f,%f,%f\n", key, minMs, result)
+}
+
+func test(key uint64) {
+	testU64(key)
+	testF64(float64(key))
 }
 
 func main() {
